7cce64ceb1309cbb4f16debdf7436a7d8ed7029f
amogh827 <hubliamogh@gmail.com>
2021-10-26 23:53:37 +0530

    Completed

--- app.js
+++ app.js
@@ -5,24 +5,27 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const _ = require("lodash");
+const mongoKey = require(__dirname + "/mongoKey.js");
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+const homeStartingContent =
+  "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const aboutContent =
+  "Hello I am Amogh Shetty, a fresh computer science graduate, possessing keen interest in developing user-friendly applications using the existing technological skillset. I am looking forward to hone my skills further for the benefit of organization and self. This is a my Blog Website which I have built.";
+const contactContent =
+  "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
-// mongoose.connect('mongodb://localhost:27017/blogDB', { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.connect('mongodb+srv://admin-amogh:amogh183@todolist.l84qa.mongodb.net/blogDB', { useNewUrlParser: true, useUnifiedTopology: true });
-
+const str = mongoKey.getKey();
+mongoose.connect(str, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const blogSchema = mongoose.Schema({
   title: String,
-  content: String
+  content: String,
 });
 
 const Blog = mongoose.model("blog", blogSchema);
@@ -30,14 +33,13 @@ const Blog = mongoose.model("blog", blogSchema);
 app.get("/", function (req, res) {
   Blog.find({}, (err, foundBlogs) => {
     let posts = [];
-    if (!err)
-      posts = foundBlogs;
+    if (!err) posts = foundBlogs;
     else {
       console.log(err);
     }
     res.render("home", {
       startingContent: homeStartingContent,
-      posts: posts
+      posts: posts,
     });
   });
 });
@@ -57,27 +59,26 @@ app.get("/compose", function (req, res) {
 app.post("/compose", async function (req, res) {
   const post = {
     title: req.body.postTitle,
-    content: req.body.postBody
+    content: req.body.postBody,
   };
 
   const newPost = new Blog({
     title: post.title,
-    content: post.content
+    content: post.content,
   });
 
   await newPost.save();
 
   res.redirect("/");
-
 });
 
 app.get("/posts/:postName", function (req, res) {
-  const requestedId = (req.params.postName);
+  const requestedId = req.params.postName;
 
   Blog.findById(requestedId, (err, post) => {
     if (!err) {
       res.render("post", {
-        post: post
+        post: post,
       });
     }
   });
@@ -91,7 +92,7 @@ app.get("/delete/:id", (req, res) => {
     }
     res.redirect("/");
   });
-})
+});
 
 app.listen(process.env.PORT || 3000, function () {
   console.log("Server started on port 3000");
---
+++ mongoKey.js
@@ -0,0 +1,3 @@
+exports.getKey = function () {
+  return "mongodb+srv://admin-amogh:amogh183@todolist.l84qa.mongodb.net/blogDB";
+};
